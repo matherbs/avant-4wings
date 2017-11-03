fed055e7cb6df1ede3ef970a0c4f7f93bfd543e3
matherbs <32384824+matherbs@users.noreply.github.com>
2017-11-03 16:20:15 +0100

    Initial commit

---
+++ landing.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <title>4 Wings Foundation</title>
+    <link rel="stylesheet" style="text.css" href="./assets/css/landing.css">
+
+  </head>
+
+  <body>
+
+                        <!-- Logo 4 Wings  -->
+    <section id="logo">
+      <img src="./assets/images/4wings.png" alt="4wings">
+    </section>
+
+                          <!-- Languages -->
+
+    <section id="languages">
+        <ul>
+
+          <li><a href="#" class="lg">
+                <span class="lgbase">Français</span>
+                <span class="lghover">Bienvenue</span>
+              </a></li>
+
+          <li><a href="#" class="lg">
+                <span class="lgbase">Nederlands</span>
+                <span class="lghover">welkom</span>
+              </a></li>
+
+          <li><a href="#" class="lg">
+                <span class="lgbase">English</span>
+                <span class="lghover">Welcome</span>
+              </a></li>
+
+        </ul>
+    </section>
+
+                      <!-- Social Media -->
+
+    <section id="social">
+        <ul>
+
+         <li><a href="https://www.instagram.com/?hl=fr" target="_blank"><img src=".\assets\icons\instagram.png" alt="instagram"></a></li>
+         <li><a href="https://fr-fr.facebook.com/" target="_blank"><img src=".\assets\icons\facebook.png" alt="facebook"></a></li>
+         <li><a href="https://twitter.com/?lang=fr" target="_blank"><img src=".\assets\icons\twitter.png" alt="twitter"></a></li>
+         <li><a href="https://www.linkedin.com/" target="_blank"><img src=".\assets\icons\linkedin.png" alt="linkedin"></a></li>
+
+       </ul>
+      </section>
+
+  </body>
+</html>
